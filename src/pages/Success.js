90662f7f847932391c7f16e36c77f75974e36ac0
Lívia Brasil <liviabrasil@id.uff.br>
2021-07-02 17:07:08 -0300

    success page

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,5 +1,32 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
 export default function Success () {
     return (
-        <> </>
+        <Container>
+        <div>
+            <h1>You'll receive a owl very soon!</h1>
+            <Link to="/home">
+                <p>Voltar para a página inicial</p> 
+            </Link>
+        </div> 
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    h1{
+        text-align: center;
+        font-size: 40px;
+        margin-bottom: 30px;
+    }
+    p{
+        font-size: 20px;
+        text-align: center;
+    }
+`
